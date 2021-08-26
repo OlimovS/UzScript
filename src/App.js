55aa7619be0008d32c1118b47d90cda2877ef30b
Sulaymon Olimov <Sulaymon_Olimov@epam.com>
2021-08-26 13:16:58 +0500

    basic working version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import {UnControlled  as CodeMirror} from 'react-codemirror2';
+import './App.css';
+
 
 function App() {
+  // const [editorState, setEditorState] = useState('console.log("Hello World");')
   return (
     <div className="App">
       <h1>
         Hello
       </h1>
+      <CodeMirror 
+         value={`//Hello
+
+
+function sayHello() {
+  return "Hello world";
+}`}
+         options={{
+          mode: 'javascript',
+          theme: 'material',
+          lineNumbers: true
+        }}
+        onChange={(editor, data, value) => {
+          console.log({editor, data, value})
+        }}
+      
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/material.css');
+require('codemirror/theme/neat.css');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/javascript/javascript.js');
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
