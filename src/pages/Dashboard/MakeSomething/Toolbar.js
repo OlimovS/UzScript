dd152142ddcf3fe243ab5f82fe40368a9dff4825
Sulaymon Olimov <Sulaymon_Olimov@epam.com>
2021-08-29 20:45:37 +0500

    toolbar improved

--- src/pages/Dashboard/MakeSomething/Toolbar.js
+++ src/pages/Dashboard/MakeSomething/Toolbar.js
@@ -1,15 +1,29 @@
+import { AiFillCaretRight, AiFillFlag } from "react-icons/ai";
+
 export function PlaygroundToolbar(props) {
   return (
     <div>
-      <button id="playgroundRunID">Run</button>
+      <div className="d-flex justify-content-center py-1 px-3">
+        <div
+          className="cursor__pointer p-1 run__icon__div"
+          id="playgroundRunID"
+        >
+          <AiFillFlag size="30" color="#05a805" />
+        </div>
+      </div>
     </div>
   );
 }
 
 export function EditorToolbar({ handleClickRunBtn }) {
   return (
-    <div>
-      <button onClick={handleClickRunBtn}>Run</button>
+    <div className="d-flex py-1 px-3">
+      <div
+        className="cursor__pointer p-1 run__icon__div ml-auto"
+        onClick={handleClickRunBtn}
+      >
+        <AiFillCaretRight size="30" color="#05a805" />
+      </div>
     </div>
   );
 }
